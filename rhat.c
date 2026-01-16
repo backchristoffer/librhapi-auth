8a10ec4e6858f1eefb9127e64ce20f345e71b489
Christoffer Back <backchristoffer@hotmail.com>
2026-01-16 16:19:48 +0100

    getting started - save point

---
+++ rhat.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <curl/curl.h>
+#include <string.h>
+
+struct Buffer {
+	char *data;
+	size_t length;
+	size_t capacity;
+};
+
+
+int main() {
+	printf("DEBUG - LAST PAGE NOTHING IS WRONG - YET");
+	printf("Buffer\n");
+	return 0;
+}
