2d9e7241474404abaf4ccb11da6cd6348a542ce9
Christoffer Back <backchristoffer@hotmail.com>
2026-01-20 17:14:41 +0100

    module ready for testing

--- rhapi_auth.c
+++ rhapi_auth.c
@@ -1,3 +1,4 @@
+#include "rhapi_auth.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <curl/curl.h>
@@ -12,7 +13,10 @@ struct Buffer {
 
 size_t write_callback(void *ptr, size_t size, size_t nmemb, struct Buffer *buffer) {
 	size_t new_length = size * nmemb;
-	buffer->data = realloc(buffer->data, buffer->length + new_length + 1);
+	char *new_ptr = realloc(buffer->data, buffer->length + new_length +1);
+	if (!new_ptr) return 0;
+
+	buffer->data = new_ptr;
 	memcpy(buffer->data + buffer->length, ptr, new_length);
 	buffer->length += new_length;
 	buffer->data[buffer->length] = '\0';
@@ -21,52 +25,42 @@ size_t write_callback(void *ptr, size_t size, size_t nmemb, struct Buffer *buffe
 }
 
 
-int main() {
-	CURL *curl = curl_easy_init();
-	
-	if (!curl) {
-		fprintf(stderr, "curl init failed\n");
-		return 1;
-	}
-	
-	
+chat* rhapi_get_token(void) {
 	char *offline_token = getenv("RH_OFFLINE_TOKEN");
-	if (offline_token == NULL) {
-		fprintf(stderr, "missing env RH_OFFLINE_TOKEN\n");
-		return 1;
+	if (!offline_token) {
+		fprintf(stderr, "ERROR: RH_OFFLINE_TOKEN not found.\n");
+		return NULL;
 	}
 
+	CURL *curl = curl_easy_init();
+	if (!curl) {
+		return NULL;
+	}
+	
+	struct Buffer response = { .data = malloc(1), .length = 0 };
 	char post_fields[4096];
 	snprintf(post_fields, sizeof(post_fields), "grant_type=refresh_token&client_id=rhsm-api&refresh_token=%s", offline_token);
 	
-	struct Buffer response = { .data = malloc(1), .length = 0 };
-	
-
-
 	curl_easy_setopt(curl, CURLOPT_URL, "https://sso.redhat.com/auth/realms/redhat-external/protocol/openid-connect/token");
 	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
 	curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
 	curl_easy_setopt(curl, CURLOPT_POSTFIELDS, post_fields);
+	
 	CURLcode result = curl_easy_perform(curl);
+	char *final_token = NULL;
 
-	if (result != CURLE_OK) {
-		fprintf(stderr, "request issue, %s\n", curl_easy_strerror(result));
-		return 1;	
-	}else{
+	if (result == CURLE_OK) {
 		struct json_object *parsed_json = json_tokener_parse(response.data);
 		struct json_object *access_token;
-
-		if (json_object_object_get_ex(parsed_json, "access_token", &access_token)) {
-			printf("%s\n", json_object_get_string(access_token));
-			
-		}else{
-			fprintf(stderr, "Error: Access token not found\n");
+		
+		if (json_object_object_get_ex(parsed, "access_token", &token_obj)) {
+			final_token = strdup(json_object_get_string(token_obj));
 		}
-		json_object_put(parsed_json);
+		json_object_put(parsed);
 	}
 
 	curl_easy_cleanup(curl);
-
 	free(response.data);
-	return 0;
+
+	return final_token;
 }
---
+++ rhapi_auth.h
@@ -0,0 +1,6 @@
+#ifndef RHAPI_AUTH_H
+#define RHAPI_AUTH_H
+
+char* rhapi_get_token(void);
+
+#endif
